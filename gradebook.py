db6e27586bcce2a2b1c686f82cd26dd935f34712
smbrown22 <smbrown22@my.waketech.edu>
2026-04-21 08:29:59 -0400

    letter_grade fully implemented

--- gradebook.py
+++ gradebook.py
@@ -1,5 +1,13 @@
 def letter_grade(score):
-    return 'A'
+    if score >= 90:
+        return 'A'
+    elif score >= 80 and score < 90:
+        return 'B'
+    elif score >= 70 and score < 80:
+        return 'C'
+    elif score >= 60 and score < 70:
+        return 'D'
+    return 'F'
 def is_passing(score):
     pass 
 def average(scores):
